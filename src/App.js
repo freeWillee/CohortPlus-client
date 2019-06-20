75e427bc66efc97854144ec1fcb96744d47ae333
freeWillee <will.ik.lee@gmail.com>
2019-06-20 09:44:36 -0600

    Create router in App (react-router)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
+import {Switch, Route, withRouter} from 'react-router-dom';
+
 import './App.css';
+import UsersIndex from './containers/UsersIndex/UsersIndex';
+import Login from './components/Login/Login';
+import NewUserPage from './containers/NewUserPage/NewUserPage';
 
 class App extends Component {
   render() {
     return (
       <div>
-        This is the App Component
+        <Switch>
+          <Route exact path="/users" component={UsersIndex}/>
+          <Route exact path="/users/new" component={NewUserPage}/>
+          <Route component={Login}/>
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
