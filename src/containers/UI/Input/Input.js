e263df4334bcbe0a064fe0d28601efef79203024
freeWillee <will.ik.lee@gmail.com>
2019-06-20 09:45:12 -0600

    Create UI components

---
+++ src/containers/UI/Input/Input.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const Input = (props) => {
+    let inputElement = null
+
+    switch(props.inputType) {
+        case ('input'):
+            inputElement = <input className={classes.inputElement}{...props}/>
+            break
+        case ('textarea'):
+            inputElement = <textarea className={classes.inputElement}{...props}/>
+            break
+        default:
+            inputElement = <input className={classes.inputElement}{...props}/>
+            break
+    }
+
+    return (
+        <div className-={classes.Input}>
+            <label className-={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    )
+}
+
+export default Input;
