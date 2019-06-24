3a2201e6c4119064adc44557f12ee082632aaa64
freeWillee <will.ik.lee@gmail.com>
2019-06-24 17:22:18 -0600

    Create sort function for Users

--- src/containers/UsersIndex/UsersIndex.js
+++ src/containers/UsersIndex/UsersIndex.js
@@ -14,33 +14,52 @@ class UsersIndex extends Component {
     
     findUserPosition = user => {
         const positionId = user.relationships.position.data.id
-        return this.props.positions.filter(position=>position.id === positionId)[0]
+        if (this.props.positions.length > 0) {
+            return this.props.positions.filter(position=>position.id === positionId)[0]
             .attributes
             .title
+        }        
+    }
+
+    // my sort function
+    sortUsers = usersArray => {
+        return usersArray.sort(function(a, b) {
+            let nameA = a.attributes.last_name.toUpperCase(); // ignore upper and lowercase
+            let nameB = b.attributes.last_name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }              
+            // names must be equal
+            return 0;
+          });
     }
 
     render() {
-        let users
-
+        let usersToRender
         if (this.props.directory.length > 0) {
-            users = this.props.directory.map(user=>{
+            
+            // DEFAULT SORT BY LAST NAME --> TO BUILD OUT FUNCTIONALITY TO SORT BY TITLE
+            let usersToMap = this.sortUsers(this.props.directory)
+            
+            usersToRender = usersToMap.map(user=>{
                 const userPosition = this.findUserPosition(user)
                 return <User 
                     key={user.id} 
                     user={user}
                     position={userPosition}
+                    editUser={(formData, ownProps) => this.props.editUser(formData, ownProps)}
                 />
             })
-        }
-        
-        console.log('[IN USERS COMPONENT]:', this.props.directory)    
-        console.log('[ARRAY OF USERS TO PASS]:', users)    
+        }   
 
         return (
             <div>
                 <h1 className={classes.Header}>Team Directory</h1>
                 <div className={classes.User}>
-                    {users}  
+                    {usersToRender}  
                 </div>
             </div>
         )
@@ -54,9 +73,10 @@ const mapStateToProps = state => {
     }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         getUsers: () => dispatch(actionCreators.getUsers()),
+        editUser: (formData) => dispatch(actionCreators.editUser(formData, ownProps)),
         fetchPositions: () => dispatch(actionCreators.getPositions())       
     }
 }
