7f7bb26ae830e3fe474d5c86695e3913b85c23ed
freeWillee <will.ik.lee@gmail.com>
2019-06-24 17:23:09 -0600

    Hookup App to routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import {Switch, Route, withRouter} from 'react-router-dom';
 
 import './App.css';
 import UsersIndex from './containers/UsersIndex/UsersIndex';
+import ProjectsIndex from './containers/ProjectsIndex/ProjectsIndex';
 import Login from './components/Login/Login';
 import NewUserPage from './containers/NewUserPage/NewUserPage';
+import NewTaskPage from './containers/NewTaskPage/NewTaskPage';
+import Navbar from './components/Navigation/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Switch>
           <Route exact path="/users" component={UsersIndex}/>
           <Route exact path="/users/new" component={NewUserPage}/>
+          <Route exact path="/tasks/new" component={NewTaskPage}/>
+          <Route exact path="/projects" component={ProjectsIndex}/>
           <Route component={Login}/>
         </Switch>
       </div>
