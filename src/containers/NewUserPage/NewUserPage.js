72433403d146c4e1e2436b2446279b428e55079f
freeWillee <will.ik.lee@gmail.com>
2019-06-25 14:29:50 -0600

    Remove get user server request on newUser form

--- src/containers/NewUserPage/NewUserPage.js
+++ src/containers/NewUserPage/NewUserPage.js
@@ -7,13 +7,12 @@ import NewUserForm from '../../components/NewUserForm/NewUserForm';
 class NewUserPage extends Component {
     componentDidMount(){
         this.props.getPositions()
-        this.props.getUsers()
     }
     
     render() {
-        let showForm = "Loading"
+        let showForm
 
-        if(this.props.positions.length > 0 && this.props.users.length > 0) { 
+        if(this.props.positions.length > 0) { 
             showForm = <NewUserForm 
                 submitFormAction={(formData, ownProps) => this.props.addNewUser(formData, ownProps)}
                 positions={this.props.positions}
@@ -32,14 +31,12 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         addNewUser: (formData) => dispatch(actionCreators.createNewUser(formData, ownProps)),
         getPositions: () => dispatch(actionCreators.getPositions()),
-        getUsers: () => dispatch(actionCreators.getUsers())
     }
 }
 
 const mapStateToProps = state => {
     return {
         positions: state.users.positions,
-        users: state.users.directory,
     }
 }
 
