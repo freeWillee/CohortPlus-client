f281ed9f2124e3aa29e777e77c8882bec7ca49f3
freeWillee <will.ik.lee@gmail.com>
2019-06-27 11:19:39 -0600

    Create Main container

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import {getCurrentUser} from './actions/currentUser'
 import './App.css';
 import UsersIndex from './containers/UsersIndex/UsersIndex';
 import ProjectsIndex from './containers/ProjectsIndex/ProjectsIndex';
-
 import NewUserPage from './containers/NewUserPage/NewUserPage';
 import NewTaskPage from './containers/NewTaskPage/NewTaskPage';
 import Navbar from './components/Navigation/Navbar/Navbar';
 import {getProjects, getUsers, getPositions} from './actions/index';
 import ProjectPage from './containers/ProjectPage/ProjectPage'
+import MainContainer from './containers/MainContainer/MainContainer';
 
 class App extends Component {
   componentDidMount() {
@@ -22,13 +22,17 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        <Switch>
-          {/* <Route exact path="/users" component={UsersIndex}/>
-          <Route exact path="/users/new" component={NewUserPage}/>
-          <Route exact path="/tasks/new" component={NewTaskPage}/>
-          <Route exact path="/projects" component={ProjectsIndex}/>
-          <Route exact path="/projects/1" render={props=> <ProjectPage {...props} projectId={"1"} />}/> */}
-        </Switch>
+        <MainContainer>
+          {this.props.currentUser ? <h1>this is my main container</h1> : <h1>You are not logged in</h1>}
+          
+          <Switch>
+            {/* <Route exact path="/users" component={UsersIndex}/>
+            <Route exact path="/users/new" component={NewUserPage}/>
+            <Route exact path="/tasks/new" component={NewTaskPage}/>
+            <Route exact path="/projects" component={ProjectsIndex}/>
+            <Route exact path="/projects/1" render={props=> <ProjectPage {...props} projectId={"1"} />}/> */}
+          </Switch>
+        </MainContainer>
       </div>
     );
   }
