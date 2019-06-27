b36992ca7777f377398f21e42d0260fd2e55fd87
freeWillee <will.ik.lee@gmail.com>
2019-06-27 10:24:49 -0600

    Update user reducer to include clear current user

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,9 +1,11 @@
 import * as actionTypes from '../constants/index';
 
-export default (state=[], action) => {
+export default (state=null, action) => {
     switch(action.type) {
         case actionTypes.SET_CURRENT_USER:
             return action.user
+        case actionTypes.CLEAR_CURRENT_USER:
+            return null
         default:
             return state
     }
