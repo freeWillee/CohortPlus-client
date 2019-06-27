bbafa37cb32da9ad659dad9676a25a13ac59cacf
freeWillee <will.ik.lee@gmail.com>
2019-06-27 11:20:05 -0600

    Clear out form on submit (login)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,6 +16,11 @@ const Login = ({loginFormData, updateLoginForm, login}) => {
     const handleSubmit = event => {
         event.preventDefault()
         login(loginFormData)
+        const updatedFormInfo = {
+            username: "",
+            password: ""
+        }
+        updateLoginForm(updatedFormInfo)
     }
 
     return (
