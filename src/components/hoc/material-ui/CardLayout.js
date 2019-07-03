004cb5b471597d2021d1cdbda070020ab14d1e02
freeWillee <will.ik.lee@gmail.com>
2019-07-03 12:22:00 -0600

    create CardLayout for material UI

---
+++ src/components/hoc/material-ui/CardLayout.js
@@ -0,0 +1,16 @@
+export const styles = {
+    card: {
+      maxWidth: 345,
+      width: 200,
+      margin: 20,
+    },
+    media: {
+      height: 140,
+      background: "center",
+      backgroundSize: "contain",
+      backgroundRepeat: "no-repeat"  
+    },
+    avatar: {
+      backgroundColor: "blue",
+    }
+  };
