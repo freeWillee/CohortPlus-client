50a7a65a2b357f52ea19984f1fd3381fd139de30
freeWillee <will.ik.lee@gmail.com>
2019-07-03 12:22:40 -0600

    Update task actions

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,3 +1,5 @@
+import {getMyTasks} from './myTasks';
+import {getProjects} from './projects';
 export const createNewTask = (formData, ownProps) => {
     return dispatch => {
         fetch(
@@ -8,7 +10,15 @@ export const createNewTask = (formData, ownProps) => {
                 },                
                 body: JSON.stringify(formData)
             })
-        .then(ownProps.history.push('/projects'))
+        .then(resp => {
+            if(resp.ok) {
+                dispatch(getMyTasks())
+                dispatch(getProjects())
+                ownProps.history.push('/my-dashboard');
+              } else {
+                throw Error(`Request rejected with the following message ${resp.status}`);
+              }
+            })
         .catch(err => {
             console.log('[CREATE_TASK_ERROR]: ', err)
         })
