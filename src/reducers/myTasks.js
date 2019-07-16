ee53827ad2e08eaa4d329487386784c986030f7d
freeWillee <will.ik.lee@gmail.com>
2019-07-16 13:35:49 -0600

    Create set project id reducer

--- src/reducers/myTasks.js
+++ src/reducers/myTasks.js
@@ -1,11 +1,24 @@
 import * as actionTypes from '../constants/index';
 
-export default (state=[], action) => {
+const initialState = {
+    directory: [],
+    filterProjectId: "",
+}
+
+export default (state=initialState, action) => {
     switch(action.type) {
         case actionTypes.SET_MY_TASKS:
-            return action.tasks
-        case actionTypes.CLEAR_MY_TASKS:
-            return []
+            return {
+                ...state,
+                directory: action.tasks
+            }
+        case actionTypes.SET_PROJECT_ID:
+            return {
+                ...state,
+                filterProjectId: action.projectId
+            }
+        case actionTypes.RESET_MY_TASKS:
+            return initialState
         default:
             return state
     }
