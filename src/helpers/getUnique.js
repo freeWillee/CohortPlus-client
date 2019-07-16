c4d7800241578f2d050fd3c59fb708b990b56b1e
freeWillee <will.ik.lee@gmail.com>
2019-07-16 13:38:46 -0600

    Create getUnique user helper function

---
+++ src/helpers/getUnique.js
@@ -0,0 +1,15 @@
+// THIS FUNCTION RETURNS AN ARRAY OF UNIQUE OBJECTS
+
+export const getUnique = (arr, comp) => {
+
+    const unique = arr
+         .map(e => e[comp])
+  
+       // store the keys of the unique objects
+      .map((e, i, final) => final.indexOf(e) === i && i)
+  
+      // eliminate the dead keys & store unique objects
+      .filter(e => arr[e]).map(e => arr[e]);
+  
+     return unique;
+  }
